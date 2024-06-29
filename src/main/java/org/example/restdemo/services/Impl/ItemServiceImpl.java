09b08be058907ed85e1946148ac0ab15481ff940
StoyanMihaylov99 <stoqn.business@gmail.com>
2024-06-29 12:50:06 +0300

    Adding CRUD operations in the Item service

--- src/main/java/org/example/restdemo/services/Impl/ItemServiceImpl.java
+++ src/main/java/org/example/restdemo/services/Impl/ItemServiceImpl.java
@@ -1,8 +1,30 @@
 package org.example.restdemo.services.Impl;
 
+import org.example.restdemo.models.dtos.ItemDTO;
 import org.example.restdemo.services.Inter.ItemService;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ItemServiceImpl implements ItemService {
+
+
+    @Override
+    public void saveItem(ItemDTO itemDTO) {
+
+    }
+
+    @Override
+    public void deleteItem(ItemDTO itemDTO) {
+
+    }
+
+    @Override
+    public ItemDTO findItemById(String id) {
+        return null;
+    }
+
+    @Override
+    public void updateItem(ItemDTO itemDTO) {
+
+    }
 }
--- src/main/java/org/example/restdemo/services/Inter/ItemService.java
+++ src/main/java/org/example/restdemo/services/Inter/ItemService.java
@@ -1,4 +1,12 @@
 package org.example.restdemo.services.Inter;
 
+import org.example.restdemo.models.dtos.ItemDTO;
+
 public interface ItemService {
+
+    void saveItem(ItemDTO itemDTO);
+
+    void deleteItem(ItemDTO itemDTO);
+    ItemDTO findItemById(String id);
+    void updateItem(ItemDTO itemDTO);
 }
